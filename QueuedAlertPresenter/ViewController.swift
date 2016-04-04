52f3c455f09500291a193ec9cab0bfb248747316
Antonio Nunes <antonio.nunes@sintraworks.com>
2016-04-04 13:00:21 +0200

    Initial Commit

---
+++ QueuedAlertPresenter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QueuedAlertPresenter
+//
+//  Created by Antonio Nunes on 04/04/16.
+//  Copyright © 2016 SintraWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
